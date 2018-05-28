ef045208b702e034a4b205ba44a2aa609ab17c44
Ariel Zhen <zhenpingping@live.com>
2018-05-28 06:21:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,15 +7,15 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.any? {|e| e.length > 4} 
+  array.any? {|e| e.length > 4}
 end
 
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each do |item| 
+  array.each do |item|
     if cheese_types.include?(item)
-      return item 
-    end 
+      return item
+    end
   end 
 end
