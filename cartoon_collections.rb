b0a7677fbe3534b294ff06ff99f26b833661502f
Ariel Zhen <zhenpingping@live.com>
2018-05-28 06:16:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,11 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)# code an argument here
-  array.collect {|x| "#{x.capitalize}" + "!"} 
+  array.collect {|x| "#{x.capitalize}" + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.any? {|e| e.length > 4} 
 end
 
 def find_the_cheese# code an argument here
